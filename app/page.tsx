1fd18ed1d44d7d2b2c0b0c727e670e41b25390bd
Appon Islam <appon2003@gmail.com>
2025-11-28 04:25:54 +0600

    home quote section added

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Footer from "@/components/root/Footer";
 import HeroSection from "@/components/root/home/HeroSection";
 import HomeAboutUs from "@/components/root/home/HomeAbout";
+import HomeQuoteSection from "@/components/root/home/HomeQuote";
 import HomeServices from "@/components/root/home/HomeServices";
 import Navbar from "@/components/root/Navbar";
 
@@ -11,6 +12,7 @@ export default function Home() {
             <HeroSection></HeroSection>
             <HomeAboutUs></HomeAboutUs>
             <HomeServices></HomeServices>
+            <HomeQuoteSection></HomeQuoteSection>
             <Footer></Footer>
         </>
     );
