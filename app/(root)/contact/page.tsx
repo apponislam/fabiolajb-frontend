17a6ee5825293f7dddfd5a07ae3a5b3e20f385c0
apponislam <11appon11@gmail.com>
2025-11-29 03:49:51 +0600

    contact us page created

---
+++ app/(root)/contact/page.tsx
@@ -0,0 +1,12 @@
+import ContactPage from "@/components/root/contact/ContactUs";
+import React from "react";
+
+const page = () => {
+    return (
+        <>
+            <ContactPage></ContactPage>
+        </>
+    );
+};
+
+export default page;
