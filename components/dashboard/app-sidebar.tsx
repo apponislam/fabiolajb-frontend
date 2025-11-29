50280786011621eb147a2cb78ced5b946eeba556
Appon Islam <appon2003@gmail.com>
2025-11-29 06:32:55 +0600

    dashboard layout created

---
+++ components/dashboard/app-sidebar.tsx
@@ -0,0 +1,61 @@
+import { Calendar, Home, Inbox, Search, Settings } from "lucide-react";
+
+import { Sidebar, SidebarContent, SidebarGroup, SidebarGroupContent, SidebarGroupLabel, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from "@/components/ui/sidebar";
+import Image from "next/image";
+
+// Menu items.
+const items = [
+    {
+        title: "Home",
+        url: "#",
+        icon: Home,
+    },
+    {
+        title: "Inbox",
+        url: "#",
+        icon: Inbox,
+    },
+    {
+        title: "Calendar",
+        url: "#",
+        icon: Calendar,
+    },
+    {
+        title: "Search",
+        url: "#",
+        icon: Search,
+    },
+    {
+        title: "Settings",
+        url: "#",
+        icon: Settings,
+    },
+];
+
+export function AppSidebar() {
+    return (
+        <Sidebar className="border-r border-[#EFEFEF] bg-white!">
+            <SidebarContent className="bg-white">
+                <SidebarGroup>
+                    <SidebarGroupLabel>
+                        <Image src="/logoblack.svg" alt="Logo Black" width={86} height={50}></Image>
+                    </SidebarGroupLabel>
+                    <SidebarGroupContent>
+                        <SidebarMenu>
+                            {items.map((item) => (
+                                <SidebarMenuItem key={item.title}>
+                                    <SidebarMenuButton asChild>
+                                        <a href={item.url}>
+                                            <item.icon />
+                                            <span>{item.title}</span>
+                                        </a>
+                                    </SidebarMenuButton>
+                                </SidebarMenuItem>
+                            ))}
+                        </SidebarMenu>
+                    </SidebarGroupContent>
+                </SidebarGroup>
+            </SidebarContent>
+        </Sidebar>
+    );
+}
