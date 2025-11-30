84549813aeacfa675691f9c48e44778366d8e94d
apponislam <11appon11@gmail.com>
2025-11-30 06:12:29 +0600

    contact page created

---
+++ app/(dashboard)/dashboard/contact/page.tsx
@@ -0,0 +1,12 @@
+import { InquiryTable } from "@/components/dashboard/contact/ContactList";
+import React from "react";
+
+const page = () => {
+    return (
+        <div className="p-4 md:p-6">
+            <InquiryTable></InquiryTable>
+        </div>
+    );
+};
+
+export default page;
---
+++ components/dashboard/contact/contactDemoData.ts
@@ -0,0 +1,136 @@
+export interface Inquiry {
+    id: string;
+    fullName: string;
+    phone: string;
+    email: string;
+    message: string;
+}
+
+export const inquiryData: Inquiry[] = [
+    {
+        id: "1",
+        fullName: "John Smith",
+        phone: "01736591645",
+        email: "john.smith@gmail.com",
+        message: "Hi, I want to clean my...",
+    },
+    {
+        id: "2",
+        fullName: "Olivia Bennett",
+        phone: "01736591645",
+        email: "oliyben@gmail.com",
+        message: "Hi, I want to clean my...",
+    },
+    {
+        id: "3",
+        fullName: "Daniel Warren",
+        phone: "01736591645",
+        email: "dwarren3@gmail.com",
+        message: "Hi, I want to clean my...",
+    },
+    {
+        id: "4",
+        fullName: "Chloe Hayes",
+        phone: "01736591645",
+        email: "chloehhyes@gmail.com",
+        message: "Hi, I want to clean my...",
+    },
+    {
+        id: "5",
+        fullName: "Marcus Reed",
+        phone: "01736591645",
+        email: "reeds777@gmail.com",
+        message: "Hi, I want to clean my...",
+    },
+    {
+        id: "6",
+        fullName: "Isabelle Clark",
+        phone: "01736591645",
+        email: "belleclark@gmail.com",
+        message: "Hi, I want to clean my...",
+    },
+    {
+        id: "7",
+        fullName: "Lucas Mitchell",
+        phone: "01736591645",
+        email: "lucamitch@gmail.com",
+        message: "Hi, I want to clean my...",
+    },
+    {
+        id: "8",
+        fullName: "Mark Wilburg",
+        phone: "01736591645",
+        email: "markwill32@gmail.com",
+        message: "Hi, I want to clean my...",
+    },
+    {
+        id: "9",
+        fullName: "Nicholas Agenn",
+        phone: "01736591645",
+        email: "nicolass009@gmail.com",
+        message: "Hi, I want to clean my...",
+    },
+    {
+        id: "10",
+        fullName: "Mia Nadinn",
+        phone: "01736591645",
+        email: "mianaddin@gmail.com",
+        message: "Hi, I want to clean my...",
+    },
+    {
+        id: "11",
+        fullName: "Noemi Villan",
+        phone: "01736591645",
+        email: "noemivill99@gmail.com",
+        message: "Hi, I want to clean my...",
+    },
+    {
+        id: "12",
+        fullName: "Noemi Villan",
+        phone: "01736591645",
+        email: "noemivill99@gmail.com",
+        message: "Hi, I want to clean my...",
+    },
+    {
+        id: "13",
+        fullName: "Noemi Villan",
+        phone: "01736591645",
+        email: "noemivill99@gmail.com",
+        message: "Hi, I want to clean my...",
+    },
+    {
+        id: "14",
+        fullName: "Noemi Villan",
+        phone: "01736591645",
+        email: "noemivill99@gmail.com",
+        message: "Hi, I want to clean my...",
+    },
+    {
+        id: "15",
+        fullName: "Noemi Villan",
+        phone: "01736591645",
+        email: "noemivill99@gmail.com",
+        message: "Hi, I want to clean my...",
+    },
+    {
+        id: "16",
+        fullName: "Noemi Villan",
+        phone: "01736591645",
+        email: "noemivill99@gmail.com",
+        message: "Hi, I want to clean my...",
+    },
+    {
+        id: "17",
+        fullName: "Noemi Villan",
+        phone: "01736591645",
+        email: "noemivill99@gmail.com",
+        message: "Hi, I want to clean my...",
+    },
+    {
+        id: "18",
+        fullName: "Noemi Villan",
+        phone: "01736591645",
+        email: "noemivill99@gmail.com",
+        message: "Hi, I want to clean my...",
+    },
+];
