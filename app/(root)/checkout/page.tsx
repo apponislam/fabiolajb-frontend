ab2889a287839217e224a90bc6539ed23c2261aa
Appon Islam <appon2003@gmail.com>
2025-12-17 05:24:29 +0600

    project completed

--- app/(root)/checkout/page.tsx
+++ app/(root)/checkout/page.tsx
@@ -1,12 +1,12 @@
 import { InvoicePreview } from "@/components/root/checkout/InVoicePreviw";
-import { PaymentForm } from "@/components/root/checkout/PaymentForm";
+// import { PaymentForm } from "@/components/root/checkout/PaymentForm";
 import React from "react";
 
 const page = () => {
     return (
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 lg:gap-12 p-4 md:p-0 lg:p-12 container mx-auto">
             <InvoicePreview />
-            <PaymentForm />
+            {/* <PaymentForm /> */}
         </div>
     );
 };
